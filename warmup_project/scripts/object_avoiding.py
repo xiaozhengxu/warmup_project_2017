888532eab8294d6acb4acd8a5d2e71491a374a71
Xiaozheng Xu <xiaozheng.xu@students.olin.edu>
2017-02-05 16:45:54 -0500

    Making obstacle avoidance work and adding a bagfile of it

--- warmup_project/scripts/object_avoiding.py
+++ warmup_project/scripts/object_avoiding.py
@@ -45,40 +45,58 @@ class ObstacleAvoid(object):
         self.cur_x = cur_pos[0]
         self.cur_y = cur_pos[1]
         self.cur_yaw = cur_pos[2]
-        if self.init_x == None:
+        if self.init_x == None or self.init_y == None:
             self.init_x = self.cur_x + 3
             self.init_y =  self.cur_y + 3
 
     def process_scan(self,msg):
         """Find the closest obstacles in front of the robot:"""
-        self.dx = self.init_x - self.cur_x
-        self.dy = self.init_y - self.cur_y
+        # self.dx = self.init_x - self.cur_x
+        # self.dy = self.init_y - self.cur_y
+        self.dx = 0
+        self.dy = 0
         if self.dx > 4:
             self.dx = 4
         if self.dy > 4:
             self.dy = 4
         #dtheta = atan2((self.init_y - self.cur_y)/(self.init_x - self.cur_x))
-
+        # found_obstacle = False
         for i, d in enumerate(msg.ranges):
-            if d < 2:
+            if d < 2 and d!=0.0:
+                # if found_obstacle == False:
+                #     found_obstacle = True
                 self.dx += -1*d*math.cos(math.radians(i))
                 self.dy += -1*d*math.sin(math.radians(i))
+        # for i, d in enumerate(msg.ranges[315:361]):
+        #     if d < 2 and d!=0.0:
+        #         # if found_obstacle == False:
+        #         #     found_obstacle = True
+        #         self.dx += -1*d*math.cos(math.radians(i))
+        #         self.dy += -1*d*math.sin(math.radians(i))
+
+        # if self.dx == 0 and self.dy == 0:
+        #     self.dx = self.init_x - self.cur_x
+        #     self.dy = self.init_y - self.cur_y
+        #     print "no obstacles"
+
+        # print "found obstacle:", found_obstacle
+
         self.dtheta = math.atan2(self.dy,self.dx)
-        self.rt_d = math.sqrt(self.dx**2 + self.dy**2)
+        # self.rt_d = math.sqrt(self.dx**2 + self.dy**2)
 
-
-        goal_marker = Marker(header = Header(frame_id = "base_link"), scale = Vector3(x = 0.1, y = 0.1, z = 0.1), 
-        pose = Pose(position = Point(x = self.init_x, y = self.init_y)), 
-
-        type = 2, color = ColorRGBA(g = 1, a = 1))
-
-        self.vis_pub.publish(goal_marker)
+        # goal_marker = Marker(header = Header(frame_id = "odom"), scale = Vector3(x = 0.1, y = 0.1, z = 0.1), 
+        # pose = Pose(position = Point(x = self.init_x+3, y = self.init_y+3)), 
+        # type = 2, color = ColorRGBA(g = 1, a = 1))
+        # self.vis_pub.publish(goal_marker)
 
     def run(self):
         r = rospy.Rate(5)
         while not rospy.is_shutdown():
-            if self.dtheta!=None and self.rt_d!=None:
-                self.pub.publish(Twist(linear=Vector3(x = self.rt_d*self.kd), angular = Vector3(z = self.dtheta*self.ka)))
+            if self.dtheta!=None:
+                self.pub.publish(Twist(linear=Vector3(x = 0.3), angular = Vector3(z = self.dtheta*self.ka)))
+                print "self.dx:", self.dx
+                print "self.dy:", self.dy
+                print "self.dtheta:", self.dtheta
 
 if __name__ == '__main__':
     node = ObstacleAvoid()
