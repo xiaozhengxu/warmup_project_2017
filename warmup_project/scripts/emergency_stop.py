a513ffd2f4434a849f7ad219482246a63cbf1c72
KatieButler <katiethetraveller@gmail.com>
2017-02-15 21:19:56 -0500

    Update emergency_stop.py

--- warmup_project/scripts/emergency_stop.py
+++ warmup_project/scripts/emergency_stop.py
@@ -9,30 +9,38 @@ from sensor_msgs.msg import LaserScan
 import rospy
 
 class Emergency_Stop(object):
-
+"""
+This class is defined to detect information from the bump sensor and stop if it is triggered.
+"""
 	def __init__(self):
+		# initialize emergency stop node
 		rospy.init_node('e_stop')
+		# subscribe to bump sensor and laser scan inputs
 		rospy.Subscriber('/bump',Bump, self.process_bump)
 		rospy.Subscriber('/scan', LaserScan, self.process_scan)
+		# publish to Twist messages to command velocity
 		self.pub = rospy.Publisher('/cmd_vel',Twist, queue_size = 10)
+		# initialize important values
 		self.is_bumped = False 
 		self.see_obstacle = False
 		self.r = rospy.Rate(10)
 
+	# process bump sensor info to determined if it has been triggered or not
 	def process_bump(self,msg):
 		if (msg.leftFront==1 or msg.leftFront==1 or msg.rightFront ==1 or msg.rightSide==1):
 			self.is_bumped = True
 		else:
 			self.is_bumped = False
 
+	# process information from the laser scan to detect obstacles in robot's path
 	def process_scan(self,msg):
-		for dist in msg.ranges[0:10]:
+		for dist in msg.ranges[0:10]: #first ten degrees clockwise from the front of the robot
 			if dist == 0.0:
 				continue
 			if dist < 1.0:
 				self.see_obstacle = True
 				return
-		for dist in msg.ranges[350:361]:
+		for dist in msg.ranges[350:361]: #first ten degrees counterclockwise from the front of the robot
 			if dist == 0.0:
 				continue
 			if dist < 1.0:
@@ -40,6 +48,7 @@ class Emergency_Stop(object):
 				return
 		self.see_obstacle = False	
 	
+	#run function that if stops the robot from moving if the bump sensor has been triggered
 	def run(self):
 		while not rospy.is_shutdown():
 			linear_msg = Vector3(x = 0)
@@ -48,8 +57,6 @@ class Emergency_Stop(object):
 			if self.is_bumped:
 				twist_msg.linear.x = 0
 				self.pub.publish(twist_msg)
-			# else:
-			# 	twist_msg.linear.x = 1
 			print twist_msg
 			self.r.sleep()
 
