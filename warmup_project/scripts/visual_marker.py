8f6440c041cc6ced95cbbd08acfd6d0a8616006e
KatieButler <katiethetraveller@gmail.com>
2017-02-17 11:43:43 -0500

    Update visual_marker.py

--- warmup_project/scripts/visual_marker.py
+++ warmup_project/scripts/visual_marker.py
@@ -4,14 +4,20 @@ from visualization_msgs.msg import Marker
 from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, Point
 import rospy
-
+"""
+Create a visualization marker at a specific point that can be viewed in rviz
+"""
+# initialize vis marker node
 rospy.init_node('vis_marker')
+# publish Marker message to visualization_marker
 pub = rospy.Publisher('visualization_marker',Marker, queue_size = 10)
 
+# Create a marker at point (1,2) in the odometry frame
 my_marker = Marker(header = Header(frame_id = "odom"), pose = Pose(position = Point(x = 1, y = 2)))        
 
 r = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+    """ Run function for publsihing the marker"""
     pub.publish(my_marker)
 
